ec3c57359ca2b2b649f9c20f04b07f7a2854c9d8
HBchevelle68 <aportante10@gmail.com>
2023-11-01 22:05:34 -0400

    adding new work

--- hello_world/hello.s
+++ hello_world/hello.s
@@ -1,18 +1,22 @@
 
 .global _start
+.text
 .align 2
 
 
 _start:
+; write
     mov x0, #1
-    adr x1, hello
-    mov x2, #13
+    adrp x1, hello
+    mov x2, #14
     mov x16, #4
     svc #80
 
+; Exit
     mov x0, #0
     mov x16, #1
     svc #80
 
-hello: .ascii "Hello World!\n"
+.data
+    hello: .asciz "Hello World!\n"
 
